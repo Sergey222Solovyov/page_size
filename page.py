18fb03273550821f57bbd1a3257c41841d1ea1b4
Sergey Solovyov <sergey.solovyov222@gmail.com>
2020-09-10 18:51:43 +0300

    Write class LinkExtractor that pulls links to resources from HTML file and counted how many bytes need download to show each page

--- page.py
+++ page.py
@@ -3,23 +3,46 @@ from html.parser import HTMLParser
 from html.entities import name2codepoint
 
 sites = ['https://stackoverflow.com/',
-         # 'https://www.computerworld.com/',
-         # 'https://habr.com/ru/news/',
-         # 'https://www.it-world.ru/it-news/market/',
+         'https://www.khanacademy.org/',
+         'https://habr.com/ru/news/',
+         'https://twitter.com/',
          ]
 
 
-class MyHTMLParser(HTMLParser):
+class LinkExtractor(HTMLParser):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.links = []
+
     def handle_starttag(self, tag, attrs):
-        print("Start tag:", tag)
-        for attr in attrs:
-            print("     attr:", attr)
+        if tag not in ('link', 'script'):
+            return
+
+        # print("Start tag:", tag)
+
+        attrs = dict(attrs)
+        if 'rel' in attrs and attrs['rel'] == 'stylesheet':
+            self.links.append(attrs['href'])
+        elif 'src' in attrs:
+            self.links.append(attrs['src'])
+
+        # for attr in attrs:
+        #     print("     attr:", attr)
 
 
 for url in sites:
+    print(f'Go {url}...')
     res = urlopen(url)
     html_data = res.read()
     html_data = html_data.decode('utf8')
     total_bytes = len(html_data)
-    parser = MyHTMLParser()
-    parser.feed(html_data)
+    extractor = LinkExtractor()
+    extractor.feed(html_data)
+    print(extractor.links)
+    for link in extractor.links:
+        print(f'\tGo {link}...')
+        res = urlopen(link)
+        extra_data = res.read()
+        total_bytes += len(extra_data)
+    print(f'For url {url} need download {total_bytes // 1024} Kb')
